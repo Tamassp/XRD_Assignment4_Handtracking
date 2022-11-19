7a95fa066787ae19434e3a646e681c9e59739144
Cîrstoiu Bogdan-Florin <62263505+weilderbeast@users.noreply.github.com>
2022-11-19 17:40:39 +0100

    move all items into one scene

--- Assignment4_Unity/Assets/Tutorial.cs
+++ Assignment4_Unity/Assets/Tutorial.cs
@@ -15,9 +15,7 @@ namespace Unity.Template.VR
         public ActiveStateSelector rock;
         public ActiveStateSelector paper;
         public ActiveStateSelector scissors;
-        private readonly Vector3 _spawnPosRock = new(59.08213f, 26.66f, -27.69f);
-        private readonly Vector3 _spawnPosPaper = new(62.20f, 26.69f, -27.69f);
-        private readonly Vector3 _spawnPosScissors = new(60.70f, 26.63f, -27.69f);
+        
         private void Start()
         {
             mainScene.SetActive(false);
